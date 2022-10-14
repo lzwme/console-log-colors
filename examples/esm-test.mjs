30ba4ec387b5030350a5cbb4caaa894078e74d34
renxia <lzwy0820@qq.com>
2022-10-14 11:38:19 +0800

    chore: test for esm

---
+++ examples/esm-test.mjs
@@ -0,0 +1,9 @@
+// node --experimental-specifier-resolution=node examples/esm-test.mjs
+
+import clc from '../';
+// import { color } from '../';
+
+console.log(Object.keys(clc));
+// console.log(color.green('green'));
+// clc.log.greenBright('log.greenBright');
+// console.log(import.meta, exports)
---
+++ src/esm.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+const clc = require('./index');
+
+exports.color = clc.color;
+exports.log = clc.log;
+exports.default = clc;
